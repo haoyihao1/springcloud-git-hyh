46b180844dfa405e90c1de019d3cb383e973bb91
haoyihao1 <2733234885@qq.com>
2020-05-24 17:25:07 +0800

    初始化

---
+++ src/main/java/com/hyh/springcloudgit/SpringcloudGitApplication.java
@@ -0,0 +1,13 @@
+package com.hyh.springcloudgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyh/springcloudgit/SpringcloudGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyh.springcloudgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
